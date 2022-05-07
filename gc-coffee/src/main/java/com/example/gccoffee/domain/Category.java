d7b9dbef746f728ad1b42fa56b24d17d7ddf3c1d
hymn-fly <rlfrmsdh1@gmail.com>
2022-05-07 10:10:29 +0900

    클론 코딩에서, 모듈 분리

--- gc-coffee/src/main/java/com/example/gccoffee/domain/Category.java
+++ gc-coffee/src/main/java/com/example/gccoffee/domain/Category.java
@@ -2,5 +2,4 @@ package com.example.gccoffee.domain;
 
 public enum Category {
     COFFEE_BEAN_PACKAGE,
-
 }
---
+++ kdt-react-order-ui/src/SummaryItem.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function SummaryItem({productName, counts}) {
+    return (
+        <div className="row">
+            <h6 className="p-0">{productName}<span className="badge bg-dark text-">{counts}개</span></h6>
+        </div>
+    )
+}
